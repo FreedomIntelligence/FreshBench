7cc55371e8c317e851f07fb1ce942c8b8ce015ed
lab_PC <2281255574@qq.com>
2024-05-31 17:17:58 +0800

    succfully run pipeline to get , gjo.jsonl, gjo.csv, gjo.json

--- src/goodjudge/make_json_for_harness.py
+++ src/goodjudge/make_json_for_harness.py
@@ -5,7 +5,7 @@ import datetime
 import pdb
 
 
-data=pd.read_csv('gjo.csv')
+data=pd.read_csv('gjo_tmp.csv')
 '''
 Question,Started_time,Closed_time,Challenges_list,Tags_list,Description,Possible_Answers_dict
 Will Representative Dean Phillips cease to be a candidate for the 2024 Democratic nomination for US president before 9 June 2024?,"Feb 09, 2024 06:00PM UTC","Mar 06, 2024 07:30PM UTC","['In the News 2024', '2024 US Election Challenge']","['Leader Entry/Exit', 'US Politics', 'Elections and Referenda']",Minnesota Representative Dean Phillips has continued his challenge to President Biden's quest for the 2024 Democratic nomination despite overwhelming odds (USA Today). Examples of what will count for resolution of this question include an official announcement that Phillips no longer seeks the Democratic Party nomination for president or that he is fully suspending his campaign.,"{'Yes': {'Correct?': True, 'Final Crowd Forecast': '86%'}, 'No': {'Correct?': False, 'Final Crowd Forecast': '14%'}}"
@@ -30,7 +30,7 @@ def get_target(x):
 
 data['target']=data['Possible_Answers_dict'].apply(get_target)
 # to json
-data.to_json('gjo.json',orient='records',lines=True)
+data.to_json('gjo_tmp.json',orient='records',lines=True)
 '''
 like this
 {"Question":"Will Representative Dean Phillips cease to be a candidate for the 2024 Democratic nomination for US president before 9 June 2024?","Started_time":"2024-02-09","Closed_time":"2024-03-06","Challenges_list":["In the News 2024","2024 US Election Challenge"],"Tags_list":["Leader Entry\/Exit","US Politics","Elections and Referenda"],"Description":"Minnesota Representative Dean Phillips has continued his challenge to President Biden's quest for the 2024 Democratic nomination despite overwhelming odds (USA Today). Examples of what will count for resolution of this question include an official announcement that Phillips no longer seeks the Democratic Party nomination for president or that he is fully suspending his campaign.","Possible_Answers_dict":{"Yes":{"Correct?":true,"Final Crowd Forecast":"86%"},"No":{"Correct?":false,"Final Crowd Forecast":"14%"}},"choices":["Yes","No"],"target":"Yes"}
